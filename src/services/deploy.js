dd00cdbd3745a0fbb5807f1ef8e5dd71b87c22d4
aekoo <717750121@qq.com>
2020-06-11 12:49:41 +0800

    update /api/

--- src/services/deploy.js
+++ src/services/deploy.js
@@ -3,25 +3,25 @@ import { jsonToFormData } from '@/utils/utils';
 
 // dict 字典
 export async function getDict(params) {
-  return request('api/GetDictionary', { params });
+  return request('/api/GetDictionary', { params });
 }
 // 添加
 export async function addDict(params) {
-  return request('api/AddDictionary', {
+  return request('/api/AddDictionary', {
     method: 'POST',
     data: jsonToFormData(params),
   });
 }
 // 修改
 export async function editDict(params) {
-  return request('api/EditDictionary', {
+  return request('/api/EditDictionary', {
     method: 'PUT',
     data: jsonToFormData(params),
   });
 }
 // 删除
 export async function deleteDict(params) {
-  return request('api/DeleteDictionary', {
+  return request('/api/DeleteDictionary', {
     method: 'DELETE',
     data: jsonToFormData(params),
   });
