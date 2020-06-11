8685f15ed63e131c6939c437119599fa9b408381
aekoo <717750121@qq.com>
2020-06-11 15:52:01 +0800

    tj

--- src/services/report.js
+++ src/services/report.js
@@ -9,7 +9,7 @@ export async function GetComplaint(params) {
 }
 
 export async function exportData(params) {
-  return request('api/download', {
+  return request('/api/download', {
     method: 'GET', // GET / POST 均可以
     data: params,
     responseType: 'blob', // 必须注明返回二进制流
